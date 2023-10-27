06e5bd7469e39a830ddcd102bc7925a09e571024
admin <truongthuongvan203@gmail.com>
2023-10-27 20:18:14 +0700

    remake

--- res_api/scripts/00-laravel-deploy.sh
+++ res_api/scripts/00-laravel-deploy.sh
@@ -3,14 +3,11 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
-echo "Caching views..."
-php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate --force
