07dc125f5fec569edec41da3ce1b71ba3d4acbeb
admin <truongthuongvan203@gmail.com>
2023-10-27 09:44:18 +0700

    res init pkg

---
+++ res_api/scripts/00-laravel-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer require laravel/breeze --dev
+php artisan breeze:install
+npm install
+composer install --no-dev --working-dir=/var/www/html
+
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
